50cf0daa874f8260cb07ddb56c3cce89c4e00dbe
parshu <parshuvmahindrakar@gmail.com>
2025-07-13 02:09:45 +0530

    Added pages and routing for pages

--- invoice-generator-client/src/App.jsx
+++ invoice-generator-client/src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MenuBar from "./components/MenuBar";
+import { Toaster } from "react-hot-toast";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import DashBoard from "./pages/DashBoard";
+import MainPage from "./pages/MainPage";
+import PreviewPage from "./pages/PreviewPage";
+
 function App() {
   return (
     <>
-      <div className="container">Application is started</div>
+      <BrowserRouter>
+        <MenuBar></MenuBar>
+        <Toaster></Toaster>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/generate" element={<MainPage />} />
+          <Route path="/preview" element={<PreviewPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
